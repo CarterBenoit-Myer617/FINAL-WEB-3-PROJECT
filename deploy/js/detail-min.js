507af20edf3919b963008a52a63b1e5f0c0120b3
CarterBenoit-Myer617 <81767200+CarterBenoit-Myer617@users.noreply.github.com>
2022-03-15 16:33:27 -0400

    yup update the stage 1 css

--- deploy/js/detail-min.js
+++ deploy/js/detail-min.js
@@ -13,4 +13,13 @@
         description: "Duis ultricies feugiat erat eget tempor. Vestibulum bibendum est eu posuere suscipit. Pellentesque tincidunt sapien vitae elit vulputate vestibulum. Proin vel molestie ex. Ut vehicula nibh eu laoreet commodo. Aenean volutpat, nibh in faucibus semper, ipsum est feugiat ex, in volutpat sapien lacus ac massa. Vivamus vel nisi et sapien ullamcorper luctus sit amet sed magna. Sed porttitor sem id eros rhoncus aliquam. Phasellus dapibus, purus a consequat venenatis, nunc diam aliquam tellus, eget congue ante risus a augue. Morbi ut massa ut turpis consequat tincidunt.",
         images: ["./img/projects/zoo/zoo-1.jpg", "./img/projects/zoo/zoo-2.jpg"]
     }];
+    window.addEventListener("load", (function () {
+        console.log(e[localStorage.getItem("indexValue")].images[1]), document.querySelector("#detail h1").innerHTML = e[localStorage.getItem("indexValue")].title, document.querySelector("#detail p").innerHTML = e[localStorage.getItem("indexValue")].description;
+        let t = this.document.querySelectorAll(".detail-imgs");
+        t.forEach(((i, u) => {
+            t[u].style.backgroundImage = "url(" + e[localStorage.getItem("indexValue")].images[u] + ")"
+        })), document.querySelector("#detail-hero").style.backgroundImage = "url(" + e[localStorage.getItem("indexValue")].images[1] + ")", document.querySelector("#moreImg").style.backgroundImage = "url(" + e[localStorage.getItem("indexValue")].images[0] + ")"
+    })), document.querySelector("button").addEventListener("click", (() => {
+        location.href = "index.html"
+    }))
 }();
